5ba18a0c2e5c0f6f005ed14740000367ca6f96fb
davalcato <davalcato@yahoo.com>
2020-08-15 17:17:15 -0700

    first commit

---
+++ HookUp/HookUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookUpApp.swift
+//  HookUp
+//
+//  Created by Daval Cato on 8/15/20.
+//
+
+import SwiftUI
+
+@main
+struct HookUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
